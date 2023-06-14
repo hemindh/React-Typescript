796aa50442c2723aedb4595a5b410dcf5e022597
Vaibhav Padsala <vaibhavpadsala.ss@smartdatainc.net>
2023-06-14 16:43:34 +0530

    First React with Typescript commit

---
+++ src/Component/FirstLearningfile.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FirstLearningfile = () => {
+  let num: number = 1000;
+  //   console.log(num);
+  return (
+    <>
+      <h2>FirstComponent</h2>
+      <p>Typescript number Datatype :- {num}</p>
+    </>
+  );
+};
+
+export default FirstLearningfile;
+
+//In Typescript DataTypes is Defined as a variable and its use like define any type like as number , string , boolean .........
